5ac9f97fde2137af0efbe5db4958eb55ed41d473
Pushparaj Pushpanayagam <ppushpanayagam@gmail.com>
2024-01-07 15:21:54 +0000

    added put and delete request

--- e2e/src/tests/PUT.test.js
+++ e2e/src/tests/PUT.test.js
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test'
 
 
-test.only('Verify PUT request test', async ({request}) => {
+test('Verify PUT request test', async ({request}) => {
 
     const response = await request.put('/api/users/2', {
         data: {
@@ -15,4 +15,8 @@ test.only('Verify PUT request test', async ({request}) => {
     expect(response.ok()).toBeTruthy();
 
     console.log(await response.text());
+    const responseData = JSON.parse(await response.text());
+    console.log(await responseData.name);
+    console.log(await responseData.job);
+    console.log(await responseData.updatedAt);
 })
